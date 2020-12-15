13667c03aa622e730f00c6f6f49b2a1713ccd349
sebastian-m-gzz <sebastian.maronas@enroutesystems.com>
2020-12-14 23:21:51 -0600

    functional endpoint

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectID } from 'mongodb'
 
-export default async() => {
+export default async function connection() {
     const baseUrl = "mongodb+srv://poker-rock-master:j3T1bIQ2T6oWiOh5@poker-rock.scwog.mongodb.net/poker-rock";
     const client = await MongoClient.connect(baseUrl);
     const database = client.db('Poker-Rock');
@@ -9,6 +9,7 @@ export default async() => {
         collections: {
             users: database.collection('Users'),
             games: database.collection('Games')
-        }
+        },
+        ObjectID: ObjectID
     };
 };
